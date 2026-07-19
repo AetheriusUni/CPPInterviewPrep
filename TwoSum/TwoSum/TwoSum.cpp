6d2e094a310756be7cbf8227e25c8b4163776a9f
Marc Raymond A. Serrano <marc.serrano8779@gmail.com>
2026-07-19 15:01:35 -0500

    TwoSum LeetCode solution with comments

--- TwoSum/TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum/TwoSum.cpp
@@ -1,20 +1,103 @@
 // TwoSum.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
 
-int main()
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum( std::vector<int>& nums, int target ) 
+    {
+        // use unordered_map to track numbers we've already seen 
+        // Key = the number we're looking at
+        // Value = the index of that number
+        // this hash map is O(1) for insertions and lookups
+        std::unordered_map<int, int> numMap;
+
+        // go through each value in the input vector
+        // i is the location of our current value in nums
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            // gets the value that, when added to the nums[1], gets us the sum that equals target
+            int complement = target - nums[i];
+
+            // if we're able to find the complement in the numMap return a vector containing the location of the compliment and i 
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+
+            // if we can't find the compliment, add to numMap the current number with the index i
+            // Key = nums[i]
+            // Val = i
+            numMap[nums[i]] = i;
+        }
+        // we were unable to find a number with a compliment to give us a two sum in the array so return an empty vector
+        return {};
+    }
+};
+
+int main() 
 {
-    std::cout << "Hello World!\n";
+    // define the input data
+    std::vector<int> nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    // create an instance of the Solution class
+    Solution solution;
+
+    // call the twoSum function and store the result
+    std::vector<int> result = solution.twoSum(nums, target);
+
+    // print the output if result is not empty, print that no solution was found otherwise
+    if ( !result.empty() ) {
+        std::cout << "Indices: [" << result[0] << ", " << result[1] << "]" << std::endl;
+    }
+    else {
+        std::cout << "No two sum solution found." << std::endl;
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// this solution is O(n) time complexity since the for loop goes through each element of the input vector in the worst case if a solution isn't found sooner
+// this solution is O(n) space complexity since the numMap can possibly store up to n-1 elements in it if the two numbers are the last two elements
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+if the input vector is sorted the two pointer approach can be used where one pointer points to the starting index (left) and the other points towards the end index (right)
+if the sum of the left and right is greater than the target, move the right pointer -1
+if the sum of the left and right is less than the target, move the left pointer +1
+if the sum matches the target, return a vector conatining {left, right}
+the two pointer approach is the same time complexity as the original solution O(n), but does not use any extra memory so the space complexity is O(1)
+*/ 
