fd7e0858be84e09ef1006af2b9331dcb951930db
Marc Raymond A. Serrano <marc.serrano8779@gmail.com>
2026-08-03 03:52:38 -0500

    added delete kth element and delete val from LL

--- DeleteFromLinkedList/DeleteFromLinkedList/DeleteFromLinkedList.cpp
+++ DeleteFromLinkedList/DeleteFromLinkedList/DeleteFromLinkedList.cpp
@@ -85,10 +85,83 @@ Node* deleteTail(Node* head)
 	return head;
 }
 
+Node* deleteKthElement(Node* head, int k)
+{
+	if (head == nullptr)
+	{
+		return nullptr;
+	}
+
+	Node* currentNode = head;
+
+	// we want to delete the first element AKA the head
+	if (k == 1)
+	{
+		head = head->next;
+		delete currentNode;
+		return head;
+	}
+
+	Node* previousNode = nullptr;
+	int elementNum = 1;
+	// travese up to the kth element
+	while (currentNode != nullptr)
+	{
+		if (elementNum == k)
+		{
+			// make the previous node point to kth element's next node
+			previousNode->next = currentNode->next;
+			// delete the kth element
+			delete currentNode;
+			break;
+		}
+		// previousNode stores the node we just looked at
+		previousNode = currentNode;
+		// look at the next node to be the current node of the next iteration
+		currentNode = currentNode->next;
+		// keep track of which element we're looking at (1 indexed)
+		elementNum++;
+	}
+
+	return head;
+}
+
+Node* deleteVal(Node* head, int val)
+{
+	if (head == nullptr)
+	{
+		return nullptr;
+	}
+
+	Node* currentNode = head;
+
+	if (head->data == val)
+	{
+		head = head->next;
+		delete currentNode;
+		return head;
+	}
+
+	Node* previousNode = nullptr;
+
+	while (currentNode != nullptr)
+	{
+		if (currentNode->data == val)
+		{
+			previousNode->next = currentNode->next;
+		}
+		previousNode = currentNode;
+		currentNode = currentNode->next;
+	}
+
+	return head;
+}
+
 void printLL(Node* head)
 {
 	if (head == nullptr)
 	{
+		std::cout << "Linked List Empty" << std::endl;
 		return;
 	}
 	Node* currentNode = head;
@@ -101,6 +174,7 @@ void printLL(Node* head)
 	std::cout << std::endl;
 }
 
+// comments of values in head are for if all of these are ran in order
 int main()
 {
 	std::vector<int> arr = { 12, 5, 8, 7 };
@@ -115,5 +189,16 @@ int main()
 	// 5 8 -- 7 is removed since it's the last element
 	head = deleteTail(head);
 	printLL(head);
+
+	// 5 -- 8 is removed since it's the second element
+	int k = 2;
+	head = deleteKthElement(head, k);
+	printLL(head);
+
+	// empty since we removed 5
+	int val = 5;
+	head = deleteVal(head, val);
+	printLL(head);
+
 	return 0;
 }
